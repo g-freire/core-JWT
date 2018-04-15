d6bb2b48bf922d965de96888f01db2f9ecdbd5ee
Gustavo Freire <gustavofreire@msn.com>
2018-04-15 08:28:48 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -17,8 +17,7 @@ namespace NetCoreJWTAuth.App
             _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -30,8 +29,8 @@ namespace NetCoreJWTAuth.App
                         ValidateAudience = true,
                         ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
-                        ValidIssuer = "yourdomain.com",
-                        ValidAudience = "yourdomain.com",
+                        ValidIssuer = "somedomain.com",
+                        ValidAudience = "somedomain.com",
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]))
                     };
                 });
@@ -39,7 +38,7 @@ namespace NetCoreJWTAuth.App
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseAuthentication();
