2c8b2b315eb31b72b1eca790d775a11c0ca61449
Gustavo Freire <gustavofreire@msn.com>
2018-04-29 09:41:27 -0300

    🎮 adding symmetric key and crypto function

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -37,10 +37,10 @@ namespace NetCoreJWTAuth.App.Controllers
                 {
                     new Claim(ClaimTypes.Name, request.Username)
                 };
-
+                // symmetric security key and crypto hash function
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
                 var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                // token payload
+                // token specs
                 var token = new JwtSecurityToken(
                     issuer: "somedomain.com",
                     audience: "somedomain.com",
